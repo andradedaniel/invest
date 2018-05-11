78f79a1a28db0d5f106688079a77e571913c5077
Daniel <daniel@Daniels-MacBook-Air.local>
2018-05-11 09:47:22 -0300

    scrap do google finance

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Murilobd\GoogleFinanceStocks\Facade\GoogleFinanceStocks;
-use Murilobd\GoogleFinanceStocks\GoogleFinanceStocksException;
+// use Murilobd\GoogleFinanceStocks\Facade\GoogleFinanceStocks;
+// use Murilobd\GoogleFinanceStocks\GoogleFinanceStocksException;
+// use Goutte\Client;
+use GuzzleHttp\Client;
 
 class HomeController extends Controller
 {
@@ -25,9 +27,59 @@ class HomeController extends Controller
      */
     public function index()
     {
-        $exchange = 'BVMF';
-        $stock = 'PETR4';
-        $stock = GoogleFinanceStocks::requestStockInfos($exchange, $stock);
+		$stock = 'PETR4';
+		$timeframe = 600;
+		$periodo='1d';
+		$url = 'http://finance.google.com/finance/getprices?q='.$stock.'&x=BVMF&p='.$periodo.'&i='.$timeframe.'&f=d,c,o,h,l,v';
+		$client = new Client();
+		$response = $client->request('GET', $url);
+		$content = $response->getBody()->getContents();
+		
+		$content = explode(PHP_EOL,$content);
+		array_splice($content, 0, 7);
+		array_splice($content, -1, 1);
+		// dd($content);
+		date_default_timezone_set('America/Sao_Paulo');
+		$timestamp = substr($content[0],1,10);
+		echo "<table style='border:1px solid black'><thead><tr><th>Data</th><th>close</th><th>high</th><th>low</th><th>open</th><th>volume</th></tr></thead>";
+		foreach ($content as $key => $linha)
+		{	
+			$parts = explode(',',$content[$key]);
+			echo "<tr>";
+			echo "<td>";
+			if ($key==0)
+				echo date("d/m/Y H:i:s", $timestamp);
+			else
+				echo date("d/m/Y H:i:s", $timestamp+($timeframe*(int)$parts[0]));
+			echo "</td>";
+			echo "<td>";
+			echo $parts[1];
+			echo "</td>";
+			echo "<td>";
+			echo $parts[2];
+			echo "</td>";
+			echo "<td>";
+			echo $parts[3];
+			echo "</td>";
+			echo "<td>";
+			echo $parts[4];
+			echo "</td>";
+			echo "<td>";
+			echo $parts[5];
+			echo "</td>";
+			echo "</tr>";
+
+		}
+		echo "</table>";
+
+
+		// dd($timestamp);
+		
+		// $content[7]=str_replace($timestamp,date("d.m.Y - H:i:s", $timestamp),$content[7]);
+// 		$date = date("d M Y H:i:s");
+// // output
+// echo strtotime($date);
+		dd();
         // dd($stock);
         echo $stock->symbol;
         echo $stock->name;
